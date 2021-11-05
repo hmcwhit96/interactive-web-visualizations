ca23d15f193ed2887d00cfec612323ed90864564
hmwchit96 <hmwhitmyer@gmail.com>
2021-11-05 17:11:43 -0400

    moved files around, beginning work on app.js

---
+++ code/static/js/app.js
@@ -0,0 +1,11 @@
+// tried using the localhost solution to d3, could not get it to work
+
+// // use d3 to read the samples.json file 
+// d3.json("http://localhost:8000/interactive-web-visualizations/code/index.html")
+//     .then(function(data){console.log(data)});
+
+// function getPlots(id) {
+
+// }
+
+// create variable for data; temporary until d3 fix
